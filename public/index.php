2313d7ff47bf42950ce89527407652bc56ce0b39
Alex Davyskiba <sanya.davyskiba@gmail.com>
2022-01-31 14:35:24 +0200

    Add phpcs

--- public/index.php
+++ public/index.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
+<?php
+
+/**
+ * @file
+ * Main entry file.
+ */
+?><!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test page</title>
 </head>
+
 <body>
     <h1>Hello, World!</h1>
     <?php
@@ -13,4 +21,5 @@
     printf('Request time: %s', $date);
     ?>
 </body>
+
 </html>
